d98be29f8a19478f7efcaf9055891c686d507e6d
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:17:35 +0530

    added new program

---
+++ ApnaCollege/Sorting/bubblesorting.java
@@ -0,0 +1,17 @@
+
+
+public class bubblesorting {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,8,3,1};
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        
+    }
+}
