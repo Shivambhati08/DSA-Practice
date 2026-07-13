1f0f573025405947c6266ce8b54d48110d15e542
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:15:14 +0530

    added new program

---
+++ ApnaCollege/Recursion/convertNumtoStr.java
@@ -0,0 +1,12 @@
+public class convertNumtoStr {
+    public static String printNumtoString(int n, String str1){
+        if(n > 0){
+            return str1;
+        }
+        
+
+    }
+    public static void main(String[] args) {
+        System.out.println(printNumtoString(2019, ""));
+    }
+}
