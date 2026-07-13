451d3cc934e15b604b0e58d4866681faabdc2d97
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:27:42 +0530

    added new program

---
+++ ApnaCollege/DynamicProgramming/Unboundedknapsack/unboundedknap.java
@@ -0,0 +1,33 @@
+public class unboundedknap {
+    public static int unboundknapsack(int[] val, int[] weight, int W){
+        int n = val.length;
+        int[][] dp = new int[n + 1][W+1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                int v = val[i-1];
+                int wt = weight[i-1];
+                if(wt <= j){
+                    int incprofit = v + dp[i][j-wt];
+                    int exprofit = dp[i-1][j];
+                    dp[i][j] = Math.max(incprofit, exprofit);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return dp[n][W];
+    }
+    public static void main(String[] args) {
+        int[] val = {15, 14, 10, 45, 30};
+        int[] weight = {2, 5, 1, 3, 4};
+        int W = 7;
+        System.out.println(unboundknapsack(val, weight, W));
+    }
+}
