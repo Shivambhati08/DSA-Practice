32fccb180ce1b597eb7e8bb335361b782677bde5
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:48:06 +0530

    added new program

---
+++ ApnaCollege/LinkedList/CollectionsLL/LLbasics.java
@@ -0,0 +1,8 @@
+
+import java.util.LinkedList;
+public class LLbasics {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        System.out.println(ll);
+    }
+}
