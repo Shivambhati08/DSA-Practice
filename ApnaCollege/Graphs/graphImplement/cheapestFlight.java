bc3be1637ca84749bcee6375e947fff024db936a
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:33:59 +0530

    added new program

---
+++ ApnaCollege/Graphs/graphImplement/cheapestFlight.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class cheapestFlight {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s, int d, int w){
+            src = s;
+            dest = d;
+            wt = w;
+        }
+    }
+    public static void createGraph(int[][] flights, ArrayList<Edge>[] graph){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < flights.length; i++) {
+            int src = flights[i][0];
+            int dest = flights[i][1];
+            int wt = flights[i][2];
+            graph[src].add(new Edge(src, dest, wt));
+        }
+    }
+    static class Info{
+        int v;
+        int cost;
+        int stops;
+        public Info(int v, int c, int s){
+            this.v = v;
+            cost = c;
+            stops = s;
+        }
+    }
+    public static int findcheapestprice(int n, int[][] flights, int src, int dst, int k){
+        ArrayList<Edge>[] graph = new ArrayList[n];
+        createGraph(flights, graph);
+
+        int[] dist = new int[n];
+        for (int i = 0; i < dist.length; i++) {
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        Queue<Info> q = new LinkedList<>();
+        q.add(new Info(src, 0, 0));
+        while(!q.isEmpty()){
+            Info curr = q.remove();
+            if(curr.stops > k){
+                break;
+            }
+            for (int i = 0; i < graph[curr.v].size(); i++) {
+                Edge e = graph[curr.v].get(i);
+
+                int v = e.dest;
+                int w = e.wt;
+
+                if(curr.cost + w < dist[v] && curr.stops <= k){
+                    dist[v] = curr.cost + w;
+                    q.add(new Info(v, dist[v], curr.stops + 1));
+                }
+            }
+        }
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+    public static void main(String[] args) {
+        int[][] flights = {{0,1,100}, {1, 2, 100}, {2, 0, 100},{1, 3, 600}, {2, 3, 200}};
+        int n = 4, src = 0, dest = 3, k = 1;
+
+        int cost = findcheapestprice(n, flights, src, dest, k);
+        System.out.println(cost);
+    }
+}
