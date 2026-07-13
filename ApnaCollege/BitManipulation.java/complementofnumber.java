ae6d924cd285ec8d0e3eea7ce040bdc84de6117e
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:24:15 +0530

    added new program

---
+++ ApnaCollege/BitManipulation.java/complementofnumber.java
@@ -0,0 +1,13 @@
+
+public class complementofnumber {
+    public static void main(String[] args) {
+        int n = 6;
+        if((n & 1) == 1){
+            System.out.println("Odd");
+        }
+        else{
+            System.out.println("Even");
+        }
+    }
+    
+}
