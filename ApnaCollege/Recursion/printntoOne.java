240d78752d866d8f7d42f3aefec2af131a654f79
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:16:38 +0530

    added new program

---
+++ ApnaCollege/Recursion/printntoOne.java
@@ -0,0 +1,12 @@
+public class printntoOne {
+    public static void fun(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.print(n+" ");
+        fun(n-1);
+    }
+    public static void main(String[] args) {
+        fun(10);
+    }
+}
