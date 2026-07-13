3928f91943e97b8eb4bfeb7bb1ee8f2f5f4d3b46
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:19:25 +0530

    trappingrainwater

---
+++ ApnaCollege/Array/trappingrainwater.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class trappingrainwater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int left[] = new int[n];
+        int right[] = new int[n];
+        left[0] = arr[0];
+        right[n-1] = arr[n-1];
+        for (int i = 1; i < n; i++) {
+                left[i] = Math.max(left[i-1], arr[i]);
+        }
+        for (int i = n-2; i >=0; i--) {
+           right[i] = Math.max(right[i+1], arr[i]);
+        }
+        int trappedwater = 0;
+        for (int i = 0; i < n; i++) {
+            int min = Math.min(left[i], right[i]);
+            trappedwater += min-arr[i];
+        }
+        System.out.println(trappedwater);
+        sc.close();
+    }
+}
