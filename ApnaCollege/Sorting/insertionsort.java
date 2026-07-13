62a83a58bd8fef500a1c92e25772e8af0eee2792
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:18:38 +0530

    added new program

---
+++ ApnaCollege/Sorting/insertionsort.java
@@ -0,0 +1,18 @@
+public class insertionsort {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 8, 3, 1};
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
