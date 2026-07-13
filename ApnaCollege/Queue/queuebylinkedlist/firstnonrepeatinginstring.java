66c7de3c2ecd3ab72a9dc106cca37ca49d1eab06
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:13:03 +0530

    added new program

---
+++ ApnaCollege/Queue/queuebylinkedlist/firstnonrepeatinginstring.java
@@ -0,0 +1,18 @@
+public class firstnonrepeatinginstring {
+    public static void main(String[] args) {
+        String str = "aabccxb";
+        int[] freq = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch-'a']++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(freq[ch-'a'] == 1){
+                System.out.println(ch);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+}
