b37d7ffed33f3177acae818165cd19ccc5eb573a
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 11:18:02 +0530

    pairsinarray

---
+++ ApnaCollege/Array/pairsinarray.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class pairsinarray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                    System.out.print("("+arr[i]+","+arr[j]+")"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
