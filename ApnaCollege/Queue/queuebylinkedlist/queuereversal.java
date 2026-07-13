00a2c4cba2bfdd892de8f38a161f8c3c436d61d1
Shivam Thakur <shivamthakur237613@gmail.com>
2026-07-13 12:14:13 +0530

    added new program

---
+++ ApnaCollege/Queue/queuebylinkedlist/queuereversal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class queuereversal {
+    public static void reversequeue(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while(!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while(!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        reversequeue(queue);
+        System.out.println(queue);
+    }
+}
